7d0cdfedbb88efcdfe61a6c6306b98321d4f2274
Rve27 <rve27github@gmail.com>
2025-12-03 02:25:30 +0700

    Mi8937: Adapt to AxionOS
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- lineage_Mi8937_4_19.mk
+++ lineage_Mi8937_4_19.mk
@@ -16,7 +16,54 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_KERNEL_VERSION := 4.19
 
 # Inherit from Mi8937 device
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, device/xiaomi/Mi8937/device.mk)
+
+# Maintainer
+AXION_MAINTAINER := Rve
+
+# Blur effect
+TARGET_ENABLE_BLUR := false
+
+# CPU
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := false
+
+# GPU
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/freq_table_mhz
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/min_clock_mhz
+
+# High Brightness Mode (HBM)
+HBM_SUPPORTED := false
+
+# Flashlight strength
+TORCH_STR_SUPPORTED := false
+
+# doze fix
+# for devices with doze/sensor related issues
+TARGET_NEEDS_DOZE_FIX := false
+
+# Charging
+BYPASS_CHARGE_SUPPORTED := false
+
+# [experimental] userspace simple lmk trial version
+# of https://github.com/kerneltoast/android_kernel_google_gs201/blob/16.0.0-sultan/drivers/android/simple_lmk.c ©kerneltoast
+# Targets devices with prebuilt kernels or uslmk support across kernels
+TARGET_USES_USLMK := false
+
+# do not enable TARGET_IS_LOW_RAM if your device ram is greater than 4gb
+# else OOM will most likely occur on operations where applications and camera can fill heap limit
+# e.g uploading video/media on apps with camera preview
+# this is a mitigation targets legacy devices 4gb below
+# Using quicken is a trade-off: here we trade clean pages for dirty pages,
+# extra cpu and battery. That's because the quicken files will be jit-ed in all
+# the processes that load of shared apk and the code cache is not shared.
+# Some notable apps that will be affected by this are gms and chrome.
+TARGET_IS_LOW_RAM := true
+
+# BCR
+TARGET_PREBUILT_BCR := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
